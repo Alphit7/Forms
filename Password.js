4879deb5272cdd39f66aae11561b1b0808039fa2
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-24 13:46:21 +0200

    adding validation

--- Password.js
+++ Password.js
@@ -1,17 +1,35 @@
+let PasswordInput = document.getElementById('pwd');
+let PasswordConfirm = document.getElementById('pwd-confirm');
+
+
 export function Password(){
-    let PasswordInput = document.getElementById('pwd');
-    let PasswordConfirm = document.getElementById('pwd-confirm');
-    PasswordConfirm.classList.add("pwd");
-    PasswordInput.classList.add("pwd");
-    let pwd = document.querySelectorAll(".pwd");
-    PasswordConfirm.addEventListener('keyup', function(){
-        if(PasswordInput.value != PasswordConfirm.value){
-            PasswordInput.style.borderColor =("red");
-            PasswordConfirm.style.borderColor =("red");
-        }
-        else{
-            PasswordInput.style.borderColor =("black");
-            PasswordConfirm.style.borderColor =("black");
-        }
-    })
+    PasswordConfirm.addEventListener('keyup', PasswordConfirmation);
+    PasswordInput.addEventListener('keyup', PasswordConfirmation);
+function PasswordConfirmation(){
+    if(PasswordInput.value != PasswordConfirm.value){
+        PasswordInput.style.borderColor =("red");
+        PasswordConfirm.style.borderColor =("red");
+    }
+    else{
+        PasswordInput.style.borderColor =("black");
+        PasswordConfirm.style.borderColor =("black");
+    }
+}
+    PasswordConfirm.addEventListener("keyup", Validation);
+    PasswordInput.addEventListener("keyup", Validation);
+    function Validation(e){
+    if(e.keyCode === 13){
+    let pwdValue = PasswordConfirm.value;
+    if(PasswordInput.value != PasswordConfirm.value){
+        alert("The password doesn't match the confirmation");
+        return;
+    }
+    else if(pwdValue ===""){
+        alert("You need a password");
+    }
+    else if(pwdValue.length < 6 && pwdValue.length > 0){
+        alert("Your password is too short")
+    }
+}
+}
 }
--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ import { AgeWarning } from "./Age.js";
 import { Password } from "../Password.js";
 
 
-
-
 Password();
 AgeWarning();
 FirstNameInput();
